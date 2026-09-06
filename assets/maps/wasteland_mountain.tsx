24b577afe529822b4871241563a75b0b2d5721f1
Andrey K. <Toren2013@yandex.ru>
2026-09-07 01:01:25 +0300

    Add Fallout-inspired wasteland tileset

---
+++ assets/maps/wasteland_mountain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="wasteland_mountain" tilewidth="64" tileheight="96" tilecount="32" columns="16">
+ <image source="../wasteland_mountain.png" width="1024" height="204"/>
+</tileset>
---
+++ assets/maps/wasteland_trees1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="wasteland_trees1" tilewidth="126" tileheight="126" tilecount="4" columns="4">
+ <image source="../wasteland_trees1.png" width="512" height="128"/>
+</tileset>
